7eb660a2387dfec61819bb01d701c979cf36e9d7
Nderim Krasniqi <Nderim.k1@gmail.com>
2020-09-21 10:38:21 +0200

    Connecting to DB with Mongoose

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route Files
-const products = require('./routes/products');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+//Route Files
+const products = require('./routes/products');
 
 const app = express();
 
@@ -18,7 +21,14 @@ app.use('/api/v1/products', products);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close Server and Exit process
+  server.close(() => process.exit(1));
+});
