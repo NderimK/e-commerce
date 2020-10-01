059d349ef818e780eda5e577fe083c18bf622400
Nderim Krasniqi <Nderim.k1@gmail.com>
2020-10-01 14:01:53 +0200

    Updated Product model and added User, Order and Review

--- models/Product.js
+++ models/Product.js
@@ -1,45 +1,52 @@
 const mongoose = require('mongoose');
+const reviewSchema = require('./Review');
 
-const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please add a name'],
-    unique: true,
-    trim: true,
-    maxlength: [25, 'Name cant be more than 50 characters'],
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: [true, 'Please add a description'],
-  },
-  price: {
-    type: Number,
-    trim: true,
-    required: true,
-    maxlength: 32,
-  },
-  category: {
-    type: String,
-    trim: true,
-    required: [true, 'Please add a category'],
-  },
-  stock: {
-    type: Number,
-    required: true,
-  },
-  sold: {
-    type: Number,
-    default: 0,
-  },
+  {
+    timestamps: true,
+  }
+);
 
-  images: {
-    type: [Buffer],
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('Product', productSchema);
