ab40fe9147e07376ca7ef06c0c9883d61c91c2c7
Nderim Krasniqi <Nderim.k1@gmail.com>
2020-10-05 03:21:17 +0200

    added homescreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
+import HomeScreen from './screens/HomeScreen';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Ecommerce</h1>
-    </div>
+    <Fragment>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
   );
-}
+};
 
 export default App;
